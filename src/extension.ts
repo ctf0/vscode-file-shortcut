1ee710dfa1734f765ed4d60f140a3c0e2a5dba50
ctf0 <muah003@gmail.com>
2019-12-31 19:29:20 +0200

    change list name

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
-import {window} from 'vscode'
+import { window } from 'vscode'
 import * as commands from './commands'
 import TreeProvider from './treeProvider'
 
-export function activate({subscriptions}) {
+export function activate({ subscriptions }) {
     subscriptions.push(commands.openFile())
     subscriptions.push(commands.addCurrentFile())
     subscriptions.push(commands.deleteFile())
     subscriptions.push(commands.showFileList())
 
-    window.registerTreeDataProvider('list', new TreeProvider())
+    window.registerTreeDataProvider('fs_list', new TreeProvider())
 }
