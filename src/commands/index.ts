863833c3c2e7b7739df2923418bc9c8ee64e935a
ctf0 <muah003@gmail.com>
2020-01-04 05:33:17 +0200

    005

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,28 +1,30 @@
-import {commands, window} from 'vscode'
+import { commands, window } from 'vscode'
 import * as util from '../utils'
-export {showFileList} from './showFileList'
+export { showFileList } from './showFileList'
 
 export function addCurrentFile() {
     return commands.registerCommand('fileShortcut.addCurrentFile', async (e) => {
-        let list: any[] = await util.getConf('list')
-        let doc = window.activeTextEditor.document
-        let path = e ? e.fsPath : doc.uri.fsPath
+        try {
+            let list: any[] = await util.getConf('list')
+            let doc = window.activeTextEditor.document
+            let path = e ? e.fsPath : doc.uri.fsPath
 
-        if (doc.isUntitled) {
-            return util.showMsg('can\'t add an "untilted" file')
+            if (doc.isUntitled) {
+                return util.showMsg('can\'t add an "untilted" file')
+            }
+
+            if (list.includes(path)) {
+                return util.showMsg('already added')
+            }
+
+            list.push(path)
+            util.updateConf('list', list)
+            util.showMsg('file added', false)
+        } catch ({ message }) {
+            util.showMsg(message, true)
         }
-
-        if (list.includes(path)) {
-            return util.showMsg('already added')
-        }
-
-        list.push(path)
-        util.updateConf('list', list)
-        util.showMsg('file added', false)
     })
 }
-
-
 export function deleteFile() {
     return commands.registerCommand('fileShortcut.deleteFile', async (e) => {
         let list: any[] = await util.getConf('list')
